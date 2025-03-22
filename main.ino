532abeccdd9edf6a910917c3bb8f5a6b34a7bf22
Zion Onwujuba <70918620+ZionOnwujuba@users.noreply.github.com>
2025-03-21 20:47:17 -0500

    main stuf

---
+++ main.ino
@@ -0,0 +1,10 @@
+//  I JUST SLAMMED MY WEINER IN THE CAR DORR
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
