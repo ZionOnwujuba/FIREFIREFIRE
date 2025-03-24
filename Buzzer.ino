8a41d540dc07764602055c7c1deba40366fb944c
mjzh0ng <157547806+mjzh0ng@users.noreply.github.com>
2025-03-24 14:49:27 -0600

    Add files via upload

---
+++ Buzzer.ino
@@ -0,0 +1,23 @@
+int buzzerPin = 13;
+int pan3Pin = 3;
+bool flag = false;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(pan3Pin, INPUT);
+}
+
+void loop() {
+  // Buzzer off, button pressed
+  if(digitalRead(pan3Pin) == HIGH && flag == false) {
+    // turn buzzer on
+    digitalWrite(buzzerPin, HIGH);
+    flag = true;
+    while(digitalRead(pan3Pin) == HIGH);
+  } else if(digitalRead(pan3Pin) && flag == true) {
+    digitalWrite(buzzerPin, LOW);
+    flag = false;
+    while(digitalRead(pan3Pin) == HIGH);
+  }
+
+}
